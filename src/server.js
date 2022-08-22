7f2c362a55660ef2a1fbf5dd3cf31ca5e8578655
hendra.susilo <hendra.susilo9025@gmail.com>
2022-08-23 01:23:55 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-console */
+/* eslint-disable import/no-unresolved */
+// console.log('Hallo kita akan membuat RESTful API')
+
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
